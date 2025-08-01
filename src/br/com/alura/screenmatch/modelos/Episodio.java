122cf22775d9ffec8cc4210a6a20c03e7f87d790
MiguelSant7 <miguelsantoss006@gmail.com>
2025-07-31 21:06:14 -0300

    Initial commit

---
+++ src/br/com/alura/screenmatch/modelos/Episodio.java
@@ -0,0 +1,43 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculo.Classificacao;
+
+public class Episodio implements Classificacao {
+    private int numero;
+    private String nome;
+    private Serie serie;
+    private int totalDeAvaliacoes;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    @Override
+    public int getClassificacao() {
+        if (totalDeAvaliacoes > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
+    }
+}
